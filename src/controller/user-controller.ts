e9060a73426658e1a9d8431d9edb941d589d714c
Felipe Borba <felipe.borba0109@gmail.com>
2024-03-27 23:36:45 -0300

    feat: add user crud

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,23 +1,55 @@
-import { Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
 
 export default class UserController {
-  async create(req: Request, res: Response, next: NextFunction) {
-    //TODO
+  prisma;
+
+  constructor() {
+    this.prisma = new PrismaClient();
   }
 
-  async update(req: Request, res: Response, next: NextFunction) {
-    //TODO
+  async create(request: Request, response: Response, next: NextFunction) {
+    const { name, email } = request.body;
+
+    const user = await this.prisma.user.create({ data: { name, email } });
+
+    response.json(user);
   }
 
-  async delete(req: Request, res: Response, next: NextFunction) {
-    //TODO
+  async update(request: Request, response: Response, next: NextFunction) {
+    const { name, email, id } = request.body;
+
+    const user = await this.prisma.user.update({
+      where: { id },
+      data: { name, email },
+    });
+
+    response.json(user);
   }
 
-  async list(req: Request, res: Response, next: NextFunction) {
-    //TODO
+  async delete(request: Request, response: Response, next: NextFunction) {
+    const { id } = request.params;
+
+    const user = await this.prisma.user.delete({
+      where: { id },
+    });
+
+    response.json(user);
   }
 
-  async getById(req: Request, res: Response, next: NextFunction) {
-    //TODO
+  async list(request: Request, response: Response, next: NextFunction) {
+    const user = await this.prisma.user.findMany();
+
+    response.json(user);
+  }
+
+  async getById(request: Request, response: Response, next: NextFunction) {
+    const { id } = request.params;
+
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    response.json(user);
   }
 }
--- src/router/user-router.ts
+++ src/router/user-router.ts
@@ -1,13 +1,29 @@
 import express from "express";
+import { body } from "express-validator";
 import UserController from "../controller/user-controller";
 
 const router = express.Router();
 const controller = new UserController();
 
-router.post("/", controller.create);
-router.put("/", controller.update);
+router.post(
+  "/",
+  body("name").optional(),
+  body("email", "email is required").notEmpty(),
+  controller.create
+);
+
+router.put(
+  "/",
+  body("id", "id is required").notEmpty(),
+  body("name").optional(),
+  body("email", "email is required").notEmpty(),
+  controller.update
+);
+
 router.delete("/:id", controller.delete);
+
 router.get("/", controller.list);
+
 router.get("/:id", controller.getById);
 
 export default router;
