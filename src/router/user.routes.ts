756e3d6a91fdaa757281a99b139fb548a4d6a68a
Felipe Borba <felipe.borba0109@gmail.com>
2024-04-07 22:02:14 -0300

    fiz validation

--- src/router/user.routes.ts
+++ src/router/user.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { body } from "express-validator";
 import UserController from "../controller/user-controller";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import validate from "../middlewares/validate";
 
 const router = Router();
 const controller = new UserController();
@@ -10,21 +12,24 @@ router.post(
   body("name").optional(),
   body("email", "email is required").notEmpty(),
   body("password", "password is required").notEmpty(),
+  validate,
   controller.create
 );
 
 router.put(
   "/",
+  ensureAuthenticated,
   body("id", "id is required").notEmpty(),
   body("name").optional(),
   body("email", "email is required").notEmpty(),
+  validate,
   controller.update
 );
 
-router.delete("/:id", controller.delete);
+router.delete("/:id", ensureAuthenticated, controller.delete);
 
-router.get("/", controller.list);
+router.get("/", ensureAuthenticated, controller.list);
 
-router.get("/:id", controller.getById);
+router.get("/:id", ensureAuthenticated, controller.getById);
 
 export default router;
--- tests/router/user.router.test.ts
+++ tests/router/user.router.test.ts
@@ -1,19 +1,50 @@
 import { PrismaClient } from "@prisma/client";
-import { sign, verify } from "jsonwebtoken";
 import supertest from "supertest";
 import { getUserTokenByEmail } from "../utils";
 
 const request = supertest("http://localhost:3333");
+const prisma = new PrismaClient();
 
 describe("User router", () => {
+  const name = "test";
+  const email = "test@email.com";
+  const password = "123123123";
+
   beforeAll(async () => {
     const token = getUserTokenByEmail("dev@email.com");
   });
 
   describe("Create user", () => {
-    test("Given all params then should create a user", async () => {});
+    beforeEach(async () => {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
 
-    test("Given no params should return error with required params", async () => {});
+      if (user) {
+        await prisma.user.delete({
+          where: { email },
+        });
+      }
+    });
+
+    test("Given all params then should create a user", async () => {
+      const result = await request
+        .post("/user/")
+        .send({ name, email, password });
+
+      expect(result.status).toBe(200);
+      expect(result.body.name).toEqual("test");
+      expect(result.body.email).toEqual("test@email.com");
+    });
+
+    test("Given no params should return error with required params", async () => {
+      const result = await request.post("/user/").send({});
+
+      expect(result.status).toBe(400);
+      expect(result.body).toEqual({
+        message: ["field email is required", "field password is required"],
+      });
+    });
   });
 
   describe("Update user", () => {
